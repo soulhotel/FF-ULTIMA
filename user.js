50a37c9c97a9351697ae6ed61c4cc305410ceccf
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-04-13 13:21:50 -0400

    Add files via upload

---
+++ user.js
@@ -0,0 +1,5 @@
+user_pref("ultima.tabs.vertical", true);
+user_pref("ultima.tabs.xs", false);
+user_pref("ultima.tabs.s", false);
+user_pref("ultima.tabs.l", true);
+user_pref("ultima.tabs.autohide", true);
