ce8a3eed1facc3ed22d2db600c974088a7985fff
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-06-13 23:10:37 -0400

    Update user.js

--- user.js
+++ user.js
@@ -1,26 +1,37 @@
-user_pref("ultima.tabs.autohide", true);
 user_pref("ultima.tabs.size.xs", false);
 user_pref("ultima.tabs.size.s", false);
 user_pref("ultima.tabs.size.l", true);
 
+user_pref("ultima.tabs.autohide", true);
+user_pref("ultima.tabs.closetabsbutton", false);
+
 user_pref("ultima.tabs.vertical.hide", false);
 user_pref("ultima.tabs.vertical.onlyprivate", false);
 
+/*--------------------------------------------------------*/
+
 user_pref("ultima.sidebar.autohide", false);
 
-user_pref("ultima.xstyle.closetabsbutton", false);
+/*--------------------------------------------------------*/
+
+user_pref("ultima.theme.extensions", true);
+
+/*--------------------------------------------------------*/
+
 user_pref("ultima.xstyle.containertabs.i", false);
 user_pref("ultima.xstyle.containertabs.ii", false);
 user_pref("ultima.xstyle.containertabs.iii", true);
 user_pref("ultima.xstyle.squared", false);
 user_pref("ultima.xstyle.urlbar.suggestions", true);
 
-user_pref("ultima.theme.extensions", true);
+/*--------------------------------------------------------*/
 
 user_pref("ultima.OS.kde", true);
 user_pref("ultima.OS.gnome", false);
 user_pref("ultima.OS.mac", false);
 user_pref("ultima.OS.linux", false);
+
+/*--------------------------------------------------------*/
 
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("svg.context-properties.content.enabled", true);
