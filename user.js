4965a1662bd0f52fe4d07faafe66027d5e6f9ae6
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-06-29 12:53:09 -0400

    Update user.js
    about config warning preference, should've been here @ file creation

--- user.js
+++ user.js
@@ -36,6 +36,7 @@ user_pref("ultima.OS.kdedecorations", false);
 /*user_pref("ultima.OS.linux", false);*/ /*Experimental*/
 
 /*other*/
+user_pref("browser.aboutConfig.showWarning", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("toolkit.tabbox.switchByScrolling", true);
