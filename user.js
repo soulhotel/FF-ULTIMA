958acd976a8b7361feaf878ace6a800aef632839
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-08-03 18:32:58 -0400

    Update user.js

--- user.js
+++ user.js
@@ -36,6 +36,7 @@ user_pref("ultima.xstyle.containertabs.i", false);
 user_pref("ultima.xstyle.containertabs.ii", false);
 user_pref("ultima.xstyle.containertabs.iii", true);
 user_pref("ultima.xstyle.squared", false);
+user_pref("ultima.xstyle.squished", false);
 user_pref("ultima.xstyle.lwtheme", false); /* Experimental. To counter addon themes that use unusual color variables. */
 user_pref("ultima.xstyle.pinnedtabs.i", false); /* Experimental. Havent decided on style yet. */
 
