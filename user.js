57ed985678422d9eefe9f6498f99b2532a86066e
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-11-15 14:29:27 -0500

    Update user.js

--- user.js
+++ user.js
@@ -75,7 +75,12 @@ user_pref("devtools.debugger.remote-enabled", true);
 user_pref("devtools.chrome.enabled", true);
 user_pref("devtools.debugger.prompt-connection", false);
 user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.has-selector.enabled", true);
 user_pref("toolkit.tabbox.switchByScrolling", false);
+user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+user_pref("widget.gtk.native-context-menus", false);
+user_pref("widget.gtk.non-native-titlebar-buttons.enabled", false);
 
 user_pref("sidebar.revamp", true);
 user_pref("sidebar.verticalTabs", true);
