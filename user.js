8b805d35d4c49bed4716219c4afdad4623467607
soulhotel <155501797+soulhotel@users.noreply.github.com>
2025-05-11 07:19:04 -0400

    Update user.js

--- user.js
+++ user.js
@@ -28,6 +28,7 @@ user_pref("ultima.navbar.autohide", false);
 user_pref("ultima.navbar.windowcontrols.carl", false);
 user_pref("ultima.navbar.windowcontrols.trafficlights", false);
 user_pref("ultima.navbar.windowcontrols.whiteout", false);
+user_pref("ultima.navbar.windowcontrols.fluent", false);
 user_pref("ultima.disable.windowcontrols.button", false);
 user_pref("ultima.xstyle.bookmarks.centered", true);
 user_pref("ultima.xstyle.bookmarks.autohide", true);
